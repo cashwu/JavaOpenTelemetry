9540c0b580c2e8169c34b639b858b00b78fa8c90
Cash <cashwugeek@icloud.com>
2024-10-01 14:08:38 +0800

    init

---
+++ src/main/java/com/demo/javaopentelemetry/JavaOpenTelemetryApplication.java
@@ -0,0 +1,13 @@
+package com.demo.javaopentelemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOpenTelemetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOpenTelemetryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/javaopentelemetry/JavaOpenTelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.javaopentelemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOpenTelemetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
